4b15756f2a3e832cbe40bc695ff0e83560ba78b7
Afriyadijogja <rian.afriyadi@gmail.com>
2025-05-14 10:01:17 +0700

    - create model books [WIP]

---
+++ __manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Rian Library",
+
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "Rian Afriyadi",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'report/course_session_report.xml',
+        # 'report/course_session_report_xlsx.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
---
+++ models/books.py
@@ -0,0 +1,10 @@
+from odoo import models, fields, api, _
+from datetime import date
+
+class Books(models.Model):
+    _name = 'perpus.books'
+    _description = 'Master Data Buku'
+    
+    name = fields.Char(string='Title')
+    description = fields.Text(string='Description')
+    total = fields.Float(string='Total')
